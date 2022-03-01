94c1d864072adcd6e94c128b231da82a866433cd
sparklegem <breanna.sipley@gmail.com>
2022-03-01 11:43:36 -0800

    add smell-test.R

---
+++ scripts/smell-test.R
@@ -0,0 +1,23 @@
+# input: raw data
+# code smell-test.R
+# output: wisdom
+
+# reference: https://medium.com/@data_datum/r-for-newbies-explore-the-iris-dataset-with-r-16d1987f9edd
+
+# load data
+data(iris)
+
+# look at first 4 lines of rows
+head(iris,4)
+
+# check out the dimensions
+dim(iris)
+
+# what are the column names?
+names(iris)
+
+# attributes
+attributes(iris)
+
+# get some summary stats
+summary(iris)
