41f1712443b4782f5cfa2b452b32df564e56298b
sparklegem <breanna.sipley@gmail.com>
2022-03-01 12:01:17 -0800

    add script for wrangle.R and export data.csv

---
+++ scripts/wrangle.R
@@ -0,0 +1,36 @@
+# input: raw data
+# code: wrangle.R
+# output: data.csv
+
+# reference: https://www.r-bloggers.com/2017/08/simple-practice-data-wrangling-the-iris-dataset/
+
+# load packages
+library(tidyverse)
+library(stringr)
+
+# load data
+data(iris)
+
+# convert to tibble
+# reference: https://tibble.tidyverse.org/
+df.iris <- as_tibble(iris)
+
+# rename variables
+# reference: https://en.wikipedia.org/wiki/Snake_case
+# pipe: Ctrl + shift + M on Windows; Cmd + Shift + M on Mac
+colnames(df.iris) <- df.iris %>%
+  colnames() %>%
+  str_to_lower() %>%
+  str_replace_all("\\.","_")
+
+# inspect data
+df.iris %>% head
+head(df.iris)
+
+# export as csv
+# practice using here
+library(here)
+# reference: http://jenrichmond.rbind.io/post/how-to-use-the-here-package/
+write.csv(write.csv(df.iris,
+                    here("data","data.csv"), 
+                    row.names = FALSE))
